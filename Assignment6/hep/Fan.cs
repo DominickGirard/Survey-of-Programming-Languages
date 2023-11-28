70bc44c73c9cb5bf40d17cdc830ce97d55ef713a
Dominick Girard <dominickgirard112@gmail.com>
2023-11-28 12:21:43 -0500

    vdv

---
+++ Assignment6/hep/Fan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Assignment
+{
+    public class Fan
+    {
+        public int speed { get; set; }
+        public double radius { get; set; }
+        public string color { get; set; }
+
+        public void Fan()
+        {
+            this.setSpeed(1);
+            this.setRadius(1.53);
+            this.setColor("green");
+        }
+
+        public void Fan(double newRadius)
+        {
+            this.setRadius(newRadius);
+        }
+
+        public String toString()
+        {
+            return String.Format("A %.2f inch {0} fan at a speed of {1}",
+            this.getRadius(), this.getColor(), this.getSpeed());
+        }
+
+    }
+}
---
+++ Assignment6/hep/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            object fan1 = new Fan();
+            fan1.Speed = 3;
+            fan1.setRadius(10.26);
+            fan1.setColor("yellow");
+            Console.WriteLine(fan1);
+
+            object fan2 = new Fan(5.79);
+            fan1.setSpeed(2);
+            fan1.setColor("blue");
+            Console.WriteLine(fan2);
+
+            object fan3 = new Fan();
+            Console.WriteLine(fan3);
+        }
+
+    }
+}
